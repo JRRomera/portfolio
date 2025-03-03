3b88e7bcf63f371714699838b36586b7a8fc988b
JRRomera <166796116+JRRomera@users.noreply.github.com>
2025-03-03 18:43:35 +0100

    Add files via upload

---
+++ proyecto_agenda/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de contactos</title>
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+
+<?php 
+
+require_once __DIR__ . '/Views/AgendaView.php';
+
+?>
+    
+</body>
+</html>
